0fd8a4c66b3f9779399f0b9dd65dab95743b5674
Den <tulipadanys@gmail.com>
2022-05-24 11:29:43 +0300

    Update bubble-sort.rb

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -2,18 +2,12 @@ def bubble_sort(array)
   new_array = array
   n = 2
   until n > new_array.length do
-    spaceship_sum = 0
     for i in 0..(new_array.length - n)
       if new_array[i] > new_array[i+1]
-        e = new_array[i]
-        new_array[i] = new_array[i+1]
-        new_array[i+1] = e
-        spaceship_sum += 1
+        new_array[i], new_array[i+1] = new_array[i+1], new_array[i]
       end
     end
     n += 1 
   end
-  p n
   new_array
-
 end
